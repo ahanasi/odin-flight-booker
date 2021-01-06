c533999e8b64e34ac978954981b032f09d8d3ff4
XXX Ahana <ahana@XXXs-Air.hsd1.ca.comcast.net>
2021-01-05 19:47:15 -0800

    Configure Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -14,3 +14,13 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
         html_tag.insert html_tag.index('>'), ' class="is-danger"'
     end
 end
+
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'odin-flights.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
